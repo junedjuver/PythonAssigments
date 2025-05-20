6bf1d13066d86b3a5e103dfade8d890cad305237
junedjuver <junedansari@hotmail.com>
2025-05-20 18:10:32 +0530

    Add files via upload

--- Task2.py
+++ Task2.py
@@ -2,4 +2,4 @@
 A=input('Enter your first name: ')
 B=input('Enter your second name: ')
 C=A+" "+B+"!"
-print(f'Hello "{C}, Welcome to the Python world')
+print(f'Hello {C}, Welcome to the Python world')
