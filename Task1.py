19241bd99da643dc19c9de6f968be4aa89a7b820
junedjuver <junedansari@hotmail.com>
2025-05-20 17:28:01 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,13 @@
+#Mathematical Operations
+A=input('enter first number: ')
+B=input('enter second number: ')
+A=float(A)
+B=float(B)
+C=A+B
+D=A-B
+E=A*B
+F=A/B
+print('Addition: ',C)
+print('Subtraction: ', D)
+print('Multiplication: ', E)
+print('Division: ', F)
